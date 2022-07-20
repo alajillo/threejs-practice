aa5258dbd5b1db15be926e571d1584e05c9a9b3e
sangjun.yoon <sangjun.yoon@knowre.com>
2022-07-21 00:05:40 +0900

    Render donut and cube

--- js/app.js
+++ js/app.js
@@ -6,8 +6,14 @@ document.body.appendChild( renderer.domElement );
 const degsToRads = deg => (deg * Math.PI) / 180.0;
 const geometry = new THREE.TorusGeometry( 5, 2, 8, 100 );
 const material = new THREE.MeshPhongMaterial( { color: 0xE633FF } );
+const geometryForCube = new THREE.BoxGeometry(3,3,3);
+const materialForCube = new THREE.MeshPhongMaterial( { color: 0xffffff } );
+const cube = new THREE.Mesh(geometryForCube, materialForCube);
 const donut = new THREE.Mesh( geometry, material );
-const axesHelper = new THREE.AxesHelper( 5 );
+donut.add(cube)
+cube.position.x = 10;
+cube.position.y = 10;
+const axesHelper = new THREE.AxesHelper( 100 );
 const color = 0xFFFFFF;
 const intensity = 1;
 const light = new THREE.DirectionalLight(color, intensity);
@@ -18,9 +24,10 @@ scene.add( axesHelper );
 camera.position.z = 50;
 
 function animate(time) {
-    time *= 0.002;
+    time *= 0.001;
     donut.rotation.x = time;
     donut.rotation.y = time;
+    donut.position.x = time * 7;
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
 }
